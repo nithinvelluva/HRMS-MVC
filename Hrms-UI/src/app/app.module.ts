8534e6228719022e9bb97739911c2bc735e4f1df
nithinvvn <nithinvelluva@gmail.com>
2019-05-04 14:52:10 +0530

    added basic pages.

--- Hrms-UI/src/app/app.module.ts
+++ Hrms-UI/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { ChangepasswordComponent } from './auth/changepassword/changepassword.component';
 import { ForgotpasswordComponent } from './auth/forgotpassword/forgotpassword.component';
+import { ResetpasswordComponent } from './auth/resetpassword/resetpassword.component';
+import { ProfileComponent } from './pages/user/profile/profile.component';
+import { UsersComponent } from './pages/admin/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ChangepasswordComponent,
-    ForgotpasswordComponent
+    ForgotpasswordComponent,
+    ResetpasswordComponent,
+    ProfileComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [LoginComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
